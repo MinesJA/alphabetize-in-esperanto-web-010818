f5cfe9fe29765e4fbf5b0fefee42bd2bdd3944d2
MinesJA <minesja@gmail.com>
2017-12-29 16:47:17 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
+ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |a|
+    a.split("").map do |letter|
+      ESPERANTO_ALPHABET.index(letter)
+    end
+  end
 end
+
+#ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+=begin
+expressions = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
+sorted_expressions = ["bonan matenon", "ĉu vi parolas esperanton", "mi amas vin", "pacon"]
+=end
